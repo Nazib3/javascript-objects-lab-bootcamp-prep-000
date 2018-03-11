003eaaa27785c26da3f4139f2f4770ba14a7ad0d
Nazibul Haque <elegant.nazib@gmail.com>
2018-03-11 04:13:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key] : value})
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+function deleteFromObjectByKey(object, key){
+  var newobj = Object.assign({}, object);
+  if(delete newobj[key]) {
+    return newobj;
+  }
+}
+function destructivelyDeleteFromObjectByKey(object){}
